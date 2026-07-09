ebf46c94daf2c16cd84aa9cfe2f7647d3591d253
Payments processing "AssetPayments <30553856+assetpayments@users.noreply.github.com>
2026-07-09 14:43:26 +0200

    Add files via upload

---
+++ upload-V2/catalog/model/extension/payment/assetpayments_v2.php
@@ -0,0 +1,31 @@
+<?php
+class ModelExtensionPaymentAssetPaymentsV2 extends Model {
+	public function getMethod($address, $total) {
+		$this->load->language('extension/payment/assetpayments_v2');
+
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_assetpayments_v2_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+
+		if ($this->config->get('payment_assetpayments_v2_total') > 0 && $this->config->get('payment_assetpayments_v2_total') > $total) {
+			$status = false;
+		} elseif (!$this->config->get('payment_assetpayments_v2_geo_zone_id')) {
+			$status = true;
+		} elseif ($query->num_rows) {
+			$status = true;
+		} else {
+			$status = false;
+		}
+
+		$method_data = array();
+
+		if ($status) {
+			$method_data = array(
+			    'code'       => 'assetpayments_v2',
+			    'title'      => $this->config->get('payment_assetpayments_v2_title') ? $this->config->get('payment_assetpayments_v2_title') : $this->language->get('text_title'),
+			    'terms'      => '',
+			    'sort_order' => $this->config->get('payment_assetpayments_v2_sort_order')
+			);
+		}
+
+		return $method_data;
+	}
+}
